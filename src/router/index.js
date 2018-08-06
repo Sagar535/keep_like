b1f8a7284e58cd810b85874a13bd2c8947d9f210
sagar <070bct535@ioe.edu.np>
2018-08-06 10:35:29 +0545

    code cleaned

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Keep from '@/components/Keep_like'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Keep_like',
+      component: Keep
     }
   ]
 })
