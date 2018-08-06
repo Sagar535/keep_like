2e2bd0bd426fbd11f7e7f0eb77c894921830072c
sagar <070bct535@ioe.edu.np>
2018-08-06 15:16:15 +0545

    list added

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Keep from '@/components/Keep_like'
+import Keep_list from '@/components/Keep_list'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Keep_like',
       component: Keep
+    },
+    {
+      path: '/list',
+      name: 'Keep_list',
+      component: Keep_list
     }
   ]
 })
